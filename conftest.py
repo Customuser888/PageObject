fd158cd4294c24cd28bcae2cb266189fde72ffa9
Customuser888 <dmitrijvysoko979@gmail.com>
2025-01-31 08:39:53 +0700

    1

--- conftest.py
+++ conftest.py
@@ -5,7 +5,14 @@ from selenium.webdriver.common.by import By
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    browser = webdriver.Firefox()
+    language = "en"
     yield browser
     print("\nquit browser..")
     browser.quit()
+
+@pytest.mark.parametrize('language', ["en"])
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
